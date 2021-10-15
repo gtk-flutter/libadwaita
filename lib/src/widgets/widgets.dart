4f69366826f646c24fbffd86142941d08df054b1
Prateek SU <pankajsunal123@gmail.com>
2021-10-15 10:29:51 +0530

    Rename gtk_theme to gnome_theme

--- lib/src/widgets/widgets.dart
+++ lib/src/widgets/widgets.dart
@@ -1,10 +1,10 @@
+export 'gnome_theme.dart';
 export 'gtk_container.dart';
 export 'gtk_header_bar_minimal.dart';
 export 'gtk_header_bar.dart';
 export 'gtk_header_button.dart';
 export 'gtk_popup_menu.dart';
 export 'gtk_sidebar.dart';
-export 'gtk_theme.dart';
 export 'gtk_two_pane.dart';
 export 'gtk_view_switcher.dart';
 export 'gtk_view_switcher_tab.dart';
