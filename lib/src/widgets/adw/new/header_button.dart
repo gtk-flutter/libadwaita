985d874fc42bb6c7ab68a916b10018b9e7173f47
jesusrp98 <jesusrope98@outlook.com>
2022-01-02 12:22:39 +0100

    Migrate `AdwHeaderButton` to `RawAdwTarget`

--- lib/src/widgets/adw/new/header_button.dart
+++ lib/src/widgets/adw/new/header_button.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:libadwaita/src/utils/colors.dart';
+import 'package:libadwaita/libadwaita.dart';
 
-enum ButtonStatus { normal, hover, tapDown }
-
-class AdwHeaderButton extends StatefulWidget {
+class AdwHeaderButton extends StatelessWidget {
   /// The icon of the button. Default size would be `17`.
   final Widget icon;
 
@@ -20,36 +18,23 @@ class AdwHeaderButton extends StatefulWidget {
     this.onPressed,
   }) : super(key: key);
 
-  @override
-  _AdwHeaderButtonState createState() => _AdwHeaderButtonState();
-}
-
-class _AdwHeaderButtonState extends State<AdwHeaderButton> {
-  var status = ButtonStatus.normal;
-
-  Color? _resolveColor() {
+  Color? _resolveBackgroundColor(BuildContext context, TargetStatus status) {
     if (Theme.of(context).brightness == Brightness.dark) {
-      if (widget.isActive) {
-        return Theme.of(context).appBarTheme.backgroundColor?.lighten(0.03);
-      }
-
       switch (status) {
-        case ButtonStatus.hover:
+        case TargetStatus.hover:
+        case TargetStatus.active:
           return Theme.of(context).appBarTheme.backgroundColor?.lighten(0.03);
-        case ButtonStatus.tapDown:
+        case TargetStatus.tapDown:
           return Theme.of(context).appBarTheme.backgroundColor?.lighten(0.20);
         default:
           return null;
       }
     } else {
-      if (widget.isActive) {
-        return Theme.of(context).appBarTheme.backgroundColor?.darken(0.05);
-      }
-
       switch (status) {
-        case ButtonStatus.hover:
+        case TargetStatus.hover:
+        case TargetStatus.active:
           return Theme.of(context).appBarTheme.backgroundColor?.darken(0.05);
-        case ButtonStatus.tapDown:
+        case TargetStatus.tapDown:
           return Theme.of(context).appBarTheme.backgroundColor?.darken(0.20);
         default:
           return null;
@@ -59,30 +44,14 @@ class _AdwHeaderButtonState extends State<AdwHeaderButton> {
 
   @override
   Widget build(BuildContext context) {
-    return MouseRegion(
-      cursor: SystemMouseCursors.click,
-      onEnter: (_) => setState(() => status = ButtonStatus.hover),
-      onExit: (_) => setState(() => status = ButtonStatus.normal),
-      child: GestureDetector(
-        onTap: widget.onPressed,
-        onTapDown: (_) => setState(() => status = ButtonStatus.tapDown),
-        onTapUp: (_) => setState(() => status = ButtonStatus.hover),
-        child: AnimatedContainer(
-          height: 34,
-          width: 34,
-          duration: const Duration(milliseconds: 200),
-          curve: Curves.ease,
-          decoration: BoxDecoration(
-            borderRadius: const BorderRadius.all(
-              Radius.circular(8.0),
-            ),
-            color: _resolveColor(),
-          ),
-          child: IconTheme.merge(
-            data: Theme.of(context).iconTheme.copyWith(size: 17),
-            child: widget.icon,
-          ),
-        ),
+    return RawAdwTarget(
+      constraints: BoxConstraints.tight(const Size.square(34)),
+      backgroundColorBuilder: _resolveBackgroundColor,
+      onPressed: onPressed,
+      isActive: isActive,
+      builder: (context, status) => IconTheme.merge(
+        data: const IconThemeData(size: 17),
+        child: icon,
       ),
     );
   }
