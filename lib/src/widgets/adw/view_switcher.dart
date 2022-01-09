a735c35e78976d6fd773682d79e1d171974e1485
jesusrp98 <jesusrope98@outlook.com>
2022-01-09 13:47:46 +0100

    Significant update to `AdwViewSwitcher`

--- lib/src/widgets/adw/view_switcher.dart
+++ lib/src/widgets/adw/view_switcher.dart
@@ -14,7 +14,8 @@ class AdwViewSwitcher extends StatelessWidget {
     required this.onViewChanged,
     required this.currentIndex,
     this.style,
-    this.height = 55,
+    @Deprecated('This parameter is no longer in use')
+        this.height = 55,
     @Deprecated(
       'This field is now ignored. '
       'This feature was deprecated after v1.0.0-rc.2',
@@ -25,22 +26,28 @@ class AdwViewSwitcher extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    ViewSwitcherStyle newStyle = style ??
-        (MediaQuery.of(context).size.width > 600
-            ? ViewSwitcherStyle.desktop
-            : ViewSwitcherStyle.mobile);
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        for (final tab in tabs.asMap().entries)
-          AdwViewSwitcherTab(
-            data: tab.value,
-            isSelected: tab.key == currentIndex,
-            onSelected:
-                currentIndex != tab.key ? () => onViewChanged(tab.key) : () {},
-            style: newStyle,
-          )
-      ],
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final newStyle = style ??
+            (constraints.maxWidth > 600
+                ? ViewSwitcherStyle.desktop
+                : ViewSwitcherStyle.mobile);
+
+        return Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            for (final tab in tabs.asMap().entries)
+              AdwViewSwitcherTab(
+                data: tab.value,
+                style: newStyle,
+                isSelected: tab.key == currentIndex,
+                onSelected: currentIndex != tab.key
+                    ? () => onViewChanged(tab.key)
+                    : null,
+              )
+          ],
+        );
+      },
     );
   }
 }
